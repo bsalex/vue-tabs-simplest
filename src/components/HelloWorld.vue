45d48c7f8449db61b33de19e07bb50fcb20c0b0e
Oleksandr Beshchuk <bs.alex.mail@gmail.com>
2017-10-09 02:54:23 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <tabs>
+    <tab>Test</tab>
+  </tabs>
+</template>
+
+<script>
+import { Tabs, Tab } from 'uiv'
+
+export default {
+  name: 'headerpanel',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    'tabs': Tabs,
+    'tab': Tab
+  }
+}
+</script>
